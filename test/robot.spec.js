5b53eb1f5e99218936f725863ed2d2d768d2289e
Luke Chambers <lukenatchambers@gmail.com>
2018-05-25 11:52:12 -0600

    Fix tests

--- test/robot.spec.js
+++ test/robot.spec.js
@@ -4,16 +4,76 @@ var request = require('supertest')(url);
 
 describe('Robot API',function() {
   before((done) => {
-    this.timeout(0);
+    this.timeout(5000);
     Sails.lift({}, (err, server) => {
-      if (err) { return done(err)};
-      done(err, server)
+      if (err) { return done(err); }
+      done(err, server);
     });
   });
 
   it('returns all robots', (done) => {
-    var req = request.get('/hosts');
-    req.end((err, res) =>{
+    var req = request.get('/api/v1/hosts');
+    req.end((err, res) => {
+      if (err) {
+        throw err;
+      }
+      console.log(res.schema);
+      done();
+    });
+  });
+
+  it('returns a single robot', (done) => {
+    var req = request.get('/api/v1/hosts/1');
+    req.end((err, res) => {
+      if (err) {
+        throw err;
+      }
+      console.log(res.schema);
+      done();
+    });
+  });
+
+  it('can edit a single robot', (done) => {
+    var req = request.patch('/api/v1/hosts/1');
+    req.send({
+      robot: {
+        current_name: 'The Man in Black',
+        intelligence_metric: 18
+      }
+    });
+    req.end((err, res) => {
+      if (err) {
+        throw err;
+      }
+      console.log(res.schema);
+      done();
+    });
+  });
+
+  it('can delete a robot', (done) => {
+    var req = request.delete('/api/v1/hosts/1');
+    req.end((err, res) => {
+      if (err) {
+        throw err;
+      }
+      console.log(res.description);
+      done();
+    });
+  });
+
+  it('can create a robot', (done) => {
+    var req = request.post('api/v1/hosts');
+    req.send({
+      robot: {
+        date_added: 'March 22nd, 2042',
+        first_active: 'May 26th, 2042',
+        current_name: 'The Man in Black',
+        height: 4.05,
+        weight: 242.05,
+        intelligence_metric: 18
+      }
+    });
+    req.end((err, res) => {
       if (err) {
         throw err;
       }
