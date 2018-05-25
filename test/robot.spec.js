0de590719a8ccb91f7f0873796d138f59c9754b0
Luke Chambers <lukenatchambers@gmail.com>
2018-05-25 11:30:53 -0600

    Fix tests

--- test/robot.spec.js
+++ test/robot.spec.js
@@ -3,32 +3,26 @@ var url = 'http://localhost:1337/';
 var request = require('supertest')(url);
 
 describe('Robot API',function() {
-
-    before(function(done) {
-        this.timeout(0);
-
-        Sails.lift({},
-            function(err,server) {
-                if(err) {
-                    done(err);
-                } else {
-                    done(err,sails);
-                }
-            });
+  before((done) => {
+    this.timeout(0);
+    Sails.lift({}, (err, server) => {
+      if (err) { return done(err)};
+      done(err, server)
     });
+  });
 
-    it("returns all robots", function(done) {
-      var req = request.get("/hosts")
-      req.end(function(err, res){
-        if (err) {
-          throw err
-        }
-        console.log(res.schema);
-        done();
-      });
+  it('returns all robots', (done) => {
+    var req = request.get('/hosts');
+    req.end((err, res) =>{
+      if (err) {
+        throw err;
+      }
+      console.log(res.schema);
+      done();
     });
+  });
 
-    after(function(done) {
-        Sails.lower(done);
-    });
+  after((done) => {
+    Sails.lower(done);
+  });
 });
